9fa9737d5ff3c5ef9419bc311f7bcb9af3a81363
zee-mzha <zee_cm@protonmail.com>
2022-08-18 10:54:35 -0600

    Added checks to make sure no invalid bits get accessed
    Fixed bug that would indicate the last byte has no bits if it took up all 64 bits

--- primeoxide_lib/src/bit_vec.rs
+++ primeoxide_lib/src/bit_vec.rs
@@ -7,40 +7,57 @@ pub struct BitVec {
 }
 
 impl BitVec {
-    pub fn new() -> BitVec {
-        BitVec {
-            data: Vec::new(),
-            bit_length: 0
-        }
-    }
-
-    pub fn with_capacity(length: usize) -> BitVec {
-        let bit_length = (length%64) as u8;
+    pub fn with_capacity(length: usize, default: bool) -> BitVec {
+        let mut bit_length = (length%64) as u8;
 
         let byte_length = if bit_length == 0 {
+            bit_length = 64;
             length/64
         }
         else {
             length/64+1
         };
 
-        let data = Vec::with_capacity(byte_length);
+        let default_value = if default { u64::MAX } else { 0 };
 
         BitVec {
-            data,
+            data: vec![default_value; byte_length],
             bit_length
         }
     }
 
     pub fn set_bit(&mut self, index: usize, value: bool) {
-        let byte_index = index/64;
-        let bit_shift = if value {
-            1u64 << (index%64)
+        let u64_index = index/64;
+        let bit_shift = (index%64) as u8;
+
+        if u64_index == self.data.len()-1 && bit_shift > self.bit_length {
+            panic!("Accessing uninitialized bits. Bit length: {}, bit accessed: {}",
+                   self.bit_length, bit_shift);
+        }
+
+        let bit_mask = if value {
+            1u64 << (bit_shift)
         }
         else {
-            !(1u64 << (index%64))
+            !(1u64 << (bit_shift))
         };
 
-        self.data[byte_index] &= bit_shift;
+        unsafe {
+            *self.data.get_unchecked_mut(u64_index) &= bit_mask;
+        }
+    }
+
+    pub fn get_bit(&self, index: usize) -> bool {
+        let u64_index = index/64;
+        let bit_shift = (index%64) as u8;
+
+        if u64_index == self.data.len()-1 && bit_shift > self.bit_length {
+            panic!("Accessing uninitialized bits. Bit length: {}, bit accessed: {}",
+                     self.bit_length, bit_shift);
+        }
+
+        let bit_mask = 1u64 << bit_shift;
+
+        unsafe { (self.data.get_unchecked(u64_index) & bit_mask) != 0 }
     }
 }
