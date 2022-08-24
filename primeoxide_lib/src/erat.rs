52cea283695dba6ae48a05721e54bca63ae5a89b
zee-mzha <zee_cm@protonmail.com>
2022-08-24 13:43:03 -0600

    more efficient calculation of primes by preallocating the exact number of bytes needed. No longer return primes found, called should use get_population_count

--- primeoxide_lib/src/erat.rs
+++ primeoxide_lib/src/erat.rs
@@ -10,11 +10,11 @@ const WHEEL210_INC: [u64; 48] = [
 const WHEEL210: [u64; 48] = [
     1, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101,
     103, 107, 109, 113, 121, 127, 131, 137, 139, 143, 149, 151, 157, 163, 167, 169, 173, 179, 181,
-    187, 191, 193, 197, 199, 209,
+    187, 191, 193, 197, 199, 209
 ];
 
 /// Performs a sieve of Eratosthenes in the number range [2..`stop`]
-pub fn sieve(stop: u64) -> (BitVec, u64) {
+pub fn sieve(stop: u64) -> BitVec {
     let wheel_size = 210;
     let wheel_len = WHEEL210_INC.len() as u64;
     let mut wheel = Wheel::new(&WHEEL210_INC);
@@ -24,12 +24,21 @@ pub fn sieve(stop: u64) -> (BitVec, u64) {
 
     // Store only numbers which exist in our wheel of choice. For now this is hardcoded to a 210
     // wheel, however this may change in the future.
-    // This calculation slightly over-allocates, but those values aren't ever touched, and at most
-    // will take up a few extra bytes, so it doesn't really matter.
-    // +4 for 4 initial primes
-    let bits = (stop / wheel_size * wheel_len + stop % wheel_size + 4) as usize;
+    // This by itself slightly under counts
+    let mut bits = (stop / wheel_size * wheel_len) as usize;
+    // Gives us the max number we can store while accounting for the fractions lost from the earlier
+    // division.
+    let mut max_num = bits as u64 / wheel_len * wheel_size;
+    // Figure out how many more bits we need without going over `stop`. The reason want an exact
+    // count of how many bits we need is because this later lets us use the very efficient popcnt
+    // instruction, or an equivalent to find all primes after the square root of `stop`
+    while max_num < stop {
+        bits += 1;
+        max_num += wheel.next_inc();
+    }
+    wheel.reset();
+
     let mut numbers = BitVec::with_capacity(bits, true);
-    let mut primes_counter = 4;
 
     if stop < u64::MAX {
         let mut num = 11;
@@ -38,7 +47,7 @@ pub fn sieve(stop: u64) -> (BitVec, u64) {
         let mut iterations = 4;
         let stop_root = (stop as f64).sqrt() as u64;
 
-        while num <= stop {
+        while num <= stop_root {
             if !numbers.get_bit(iterations) {
                 num += wheel.next_inc();
                 iterations += 1;
@@ -46,28 +55,27 @@ pub fn sieve(stop: u64) -> (BitVec, u64) {
             }
 
             // Only get here if no factors were found
-            if num <= stop_root {
-                multiplier_wheel.sync(&wheel);
-                let mut multiplier = num;
-                let mut result = multiplier * num;
-                while result <= stop {
-                    let index = get_num_idx(result);
-                    numbers.set_bit(index, false);
+            multiplier_wheel.sync(&wheel);
+            let mut multiplier = num;
+            let mut result = multiplier * num;
+            while result <= stop {
+                let index = get_num_idx(result);
+                numbers.set_bit(index, false);
 
-                    multiplier += multiplier_wheel.next_inc();
-                    result = multiplier * num;
-                }
+                multiplier += multiplier_wheel.next_inc();
+                result = multiplier * num;
             }
 
             num += wheel.next_inc();
-            primes_counter += 1;
             iterations += 1;
         }
 
-        numbers.resize(iterations, false);
+        // all non primes have been marked off at this point. Let bit_vec take care of population
+        // counting the rest of the vector.
+        numbers.get_population_count(Some(iterations as usize));
     }
 
-    (numbers, primes_counter)
+    numbers
 }
 
 /// Get the index of the number passed
